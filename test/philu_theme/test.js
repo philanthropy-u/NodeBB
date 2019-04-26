2a8a81c221b94fdf0c8d333879b72e64303f14d8
segmentationfaulter <muhammadsackib@gmail.com>
2019-04-26 16:03:23 +0500

    test

---
+++ test/philu_theme/test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var theme = require('nodebb-theme-philu-community');
+
+describe('Testing', function () {
+	it('should work', function () {
+		console.log('Working');
+	});
+
+	it('should fail', function () {
+		throw new Error();
+	});
+});
