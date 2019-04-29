f6eb04ff242c881dd29396bde46d1d975f420e7a
segmentationfaulter <muhammadsackib@gmail.com>
2019-04-29 13:33:21 +0500

    log url

--- test/philu-theme.js
+++ test/philu-theme.js
@@ -6,7 +6,9 @@ var nconf = require('nconf');
 
 describe('Philu theme', function () {
 	it('should work', function () {
-		request(nconf.get('url') + '/api/v2/category/featured', function (err, res, done) {
+		var url = nconf.get('url') + '/api/v2/category/featured';
+		console.log('URLLLLL', url);
+		request(url, function (err, res, done) {
 			assert.ifError(err);
 			assert.equal(res.statusCode, 200);
 			done();
