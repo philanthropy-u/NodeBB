8709cff8c085e87ab368473a2125dfd558c41f92
segmentationfaulter <muhammadsackib@gmail.com>
2019-04-29 12:54:28 +0500

    fix invalid protocol error

--- test/philu-theme.js
+++ test/philu-theme.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 
 describe('Philu theme', function () {
 	it('should work', function () {
-		request('localhost:4567/api/v2/category/featured', function (err, res, done) {
+		request('http://localhost:4567/api/v2/category/featured', function (err, res, done) {
 			assert.ifError(err);
 			assert.equal(res.statusCod, 200);
 			done();
