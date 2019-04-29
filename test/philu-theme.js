9e9137b8dd8130e33a24328881d5cff1b500e533
segmentationfaulter <muhammadsackib@gmail.com>
2019-04-29 13:30:58 +0500

    using nconf

--- test/philu-theme.js
+++ test/philu-theme.js
@@ -2,12 +2,13 @@
 
 var request = require('request');
 var assert = require('assert');
+var nconf = require('nconf');
 
 describe('Philu theme', function () {
 	it('should work', function () {
-		request('http://localhost:4567/api/v2/category/featured', function (err, res, done) {
+		request(nconf.get('url') + '/api/v2/category/featured', function (err, res, done) {
 			assert.ifError(err);
-			assert.equal(res.statusCod, 200);
+			assert.equal(res.statusCode, 200);
 			done();
 		});
 	});
