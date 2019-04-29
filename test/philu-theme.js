3b6b4b78937eb425020b01c6b350107d4abea74a
segmentationfaulter <muhammadsackib@gmail.com>
2019-04-29 13:56:38 +0500

    add a test if philu theme is loaded or not

--- test/philu-theme.js
+++ test/philu-theme.js
@@ -3,8 +3,22 @@
 var request = require('request');
 var assert = require('assert');
 var nconf = require('nconf');
+var path = require('path');
+var plugins = require('../src/plugins');
 
 describe('Philu theme', function () {
+	it('It should load Philu theme', function (done) {
+		var pluginId = 'nodebb-theme-philu-community';
+		plugins.loadPlugin(path.join(nconf.get('base_dir'), 'node_modules/' + pluginId), function (err) {
+			assert.ifError(err);
+			assert(plugins.libraries[pluginId]);
+			assert(plugins.loadedHooks['static:app.load']);
+
+			done();
+		});
+	});
+
+	/*
 	it('should work', function () {
 		var url = nconf.get('url') + '/api/v2/category/featured';
 		console.log('URLLLLL', url);
@@ -14,4 +28,5 @@ describe('Philu theme', function () {
 			done();
 		});
 	});
+	*/
 });
